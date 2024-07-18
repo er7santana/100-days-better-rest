9a95368506bd0cba4c0f4375aec7fdcef396fa91
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-17 22:38:39 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
